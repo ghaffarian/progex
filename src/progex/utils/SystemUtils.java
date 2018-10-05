0a578afef62f4b38780071ca6bffa9d7062bba65
Seyed Mohammad Ghaffarian <s.m.ghaffarian@gmail.com>
2018-10-06 00:10:48 +0330

    Adds new getMemoryStats method

--- src/progex/utils/SystemUtils.java
+++ src/progex/utils/SystemUtils.java
@@ -30,4 +30,23 @@ public class SystemUtils {
 		out.println(memory.toString());
 	}
 	
+	/**
+	 * Get human-readable statistics about the system-memory as a String.
+	 */
+	public static String getMemoryStats() {
+		long free = Runtime.getRuntime().freeMemory();
+		long total = Runtime.getRuntime().totalMemory();
+		long max = Runtime.getRuntime().maxMemory();
+		StringBuilder memory = new StringBuilder();
+		memory.append(String.format(
+				"\n  Used Memory   : %6.2f MB  (%%%04.1f)\n"
+				+ "  Free Memory   : %6.2f MB  (%%%04.1f)\n"
+				+ "  Current Heap  : %6.2f MB  (%%%04.1f)\n"
+				+ "  Max Heap Size : %6.2f MB\n",
+				(float) (total - free) / (1024 * 1024), (float) 100 * (total - free) / total,
+				(float) free / (1024 * 1024), (float) (free * 100) / total,
+				(float) total / (1024 * 1024), (float) (total * 100) / max,
+				(float) max / (1024 * 1024)));
+		return memory.toString();
+	}	
 }
