3a2e61549e11e0b12333a49c9060c401523209c2
Seyed Mohammad Ghaffarian <s.m.ghaffarian@gmail.com>
2018-12-19 15:41:46 +0330

    Add clean-up + PDG test code

--- src/test/java/progex/AnalysisTest.java
+++ src/test/java/progex/AnalysisTest.java
@@ -1,9 +1,11 @@
 /*** In The Name of Allah ***/
 package progex;
 
+import java.io.File;
+import progex.utils.FileUtils;
+
 import static org.junit.Assert.*;
 import org.junit.*;
-import progex.utils.FileUtils;
 
 /**
  *
@@ -11,25 +13,51 @@ import progex.utils.FileUtils;
  */
 public class AnalysisTest {
     
+    private final String SRC_DIR = "src/test/resources/progex";
+    
+    @BeforeClass
+    public static void cleanUp() {
+        File out = new File("out/");
+        for (File file: out.listFiles()) {
+            if (file.isFile())
+                file.delete();
+            else
+                deleteDir(file);
+        }
+    }
+    
+    private static void deleteDir(File dir) {
+        if (dir.list().length > 0) {
+            for (File file: dir.listFiles()) {
+                if (file.isFile())
+                    file.delete();
+                else
+                    deleteDir(file);
+            }
+        }
+        dir.delete();
+    }
+    
     @Test
     public void javaCFGTest() {
-        String srcDir = "src/test/resources/progex";
-        String[] args = {"-cfg", "-outdir", "out", srcDir};
+        String[] args = {"-cfg", "-outdir", "out/cfg/", SRC_DIR};
         Main.main(args);
         //
-        String[] testFiles = FileUtils.listFilesWithSuffix(new String[] {srcDir}, Execution.Languages.JAVA.suffix);
-        String[] outFiles = FileUtils.listFilesWithSuffix(new String[]{"out"}, "-CFG.dot");
+        String[] testFiles = FileUtils.listFilesWithSuffix(new String[] {SRC_DIR}, Execution.Languages.JAVA.suffix);
+        String[] outFiles = FileUtils.listFilesWithSuffix(new String[]{"out/cfg/"}, "-CFG.dot");
         assertEquals(testFiles.length, outFiles.length);
     }
     
-//    @Test
-//    public void javaPDGTest() {
-//        
-//    }
-//    
-//    @Test
-//    public void javaASTest() {
-//        
-//    }
-
+    @Test
+    public void javaPDGTest() {
+        String[] args = {"-pdg", "-outdir", "out/pdg/", SRC_DIR};
+        Main.main(args);
+        //
+        String[] testFiles = FileUtils.listFilesWithSuffix(new String[] {SRC_DIR}, Execution.Languages.JAVA.suffix);
+        String[] outDataFiles = FileUtils.listFilesWithSuffix(new String[]{"out/pdg/"}, "-PDG-DATA.dot");
+        String[] outCtrlFiles = FileUtils.listFilesWithSuffix(new String[]{"out/pdg/"}, "-PDG-CTRL.dot");
+        assertEquals(testFiles.length, outDataFiles.length);
+        assertEquals(testFiles.length, outCtrlFiles.length);
+    }
+    
 }
