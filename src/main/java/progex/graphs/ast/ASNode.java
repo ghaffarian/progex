0e33c87205229e2cbde0701c0992d3f9b1e16fac
Seyed Mohammad Ghaffarian <s.m.ghaffarian@gmail.com>
2018-12-27 20:29:14 +0330

    Implement support for Abstract Syntax Tree (AST)

--- src/main/java/progex/graphs/ast/ASNode.java
+++ src/main/java/progex/graphs/ast/ASNode.java
@@ -1,6 +1,10 @@
 /*** In The Name of Allah ***/
 package progex.graphs.ast;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Class type of Abstract Syntax (AS) nodes.
  * 
@@ -8,4 +12,114 @@ package progex.graphs.ast;
  */
 public class ASNode {
 	
+    /**
+     * Enumeration of different types for AS nodes.
+     */
+    public enum Type {
+        ROOT        ("ROOT"),
+        IMPORTS     ("IMPORTS"),
+        IMPORT      ("IMPORT"),
+        PACKAGE     ("PACKAGE"),
+        NAME        ("NAME"),
+        MODIFIER    ("MODIFIER"),
+        CLASS       ("CLASS"),
+        EXTENDS     ("EXTENDS"),
+        IMPLEMENTS  ("IMPLEMENTS"),
+        INTERFACE   ("INTERFACE"),
+        STATIC_BLOCK("STATIC-BLOCK"),
+        CONSTRUCTOR ("CONSTRUCTOR"),
+        FIELD       ("FIELD"),
+        TYPE        ("TYPE"),
+        METHOD      ("METHOD"),
+        RETURN      ("RETURN"),
+        PARAMS      ("PARAMS"),
+        BLOCK       ("BLOCK"),
+        IF          ("IF"),
+        CONDITION   ("COND"),
+        THEN        ("THEN"),
+        ELSE        ("ELSE"),
+        VARIABLE    ("VAR"),
+        INIT_VALUE  ("INIT"),
+        STATEMENT   (""),
+        FOR         ("FOR"),
+        FOR_INIT    ("INIT"),
+        FOR_UPDATE  ("UPDATE"),
+        FOR_EACH    ("FOR-EACH"),
+        WHILE       ("WHILE"),
+        DO_WHILE    ("DO-WHILE"),
+        TRY         ("TRY"),
+        RESOURCES   ("RESOURCES"),
+        CATCH       ("CATCH"),
+        FINALLY     ("FINALLY"),
+        SWITCH      ("SWITCH"),
+        CASE        ("CASE"),
+        DEFAULT     ("DEFAULT"),
+        LABELED     ("LABELED");
+
+        public final String label;
+
+        private Type(String lbl) {
+            label = lbl;
+        }
+
+        @Override
+        public String toString() {
+            return label;
+        }
+    }
+
+    
+    private Map<String, Object> properties;
+
+    public ASNode(Type type) {
+        properties = new LinkedHashMap<>();
+        setLineOfCode(0);
+        setType(type);
+    }
+
+    public void setType(Type type) {
+        properties.put("type", type);
+    }
+
+    public Type getType() {
+        return (Type) properties.get("type");
+    }
+
+    public void setLineOfCode(int line) {
+        properties.put("line", line);
+    }
+
+    public int getLineOfCode() {
+        return (Integer) properties.get("line");
+    }
+
+    public void setValue(String code) {
+        properties.put("value", code);
+    }
+
+    public String getValue() {
+        return (String) properties.get("value");
+    }
+
+    public void setProperty(String key, Object value) {
+        properties.put(key.toLowerCase(), value);
+    }
+
+    public Object getProperty(String key) {
+        return properties.get(key.toLowerCase());
+    }
+
+    public Set<String> getAllKeys() {
+        return properties.keySet();
+    }
+
+    @Override
+    public String toString() {
+        String value = getValue();
+        if (value == null)
+            return getType().label;
+        if (getType().label.isEmpty())
+            return value;
+        return getType().label + ": " + value;
+    }
 }
--- src/main/java/progex/graphs/ast/AbstractSyntaxTree.java
+++ src/main/java/progex/graphs/ast/AbstractSyntaxTree.java
@@ -1,7 +1,16 @@
 /*** In The Name of Allah ***/
 package progex.graphs.ast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import progex.graphs.Edge;
 import progex.graphs.Graph;
+import progex.utils.Logger;
+import progex.utils.StringUtils;
 
 /**
  * Abstract Syntax Tree (AST).
@@ -9,13 +18,87 @@ import progex.graphs.Graph;
  * @author Seyed Mohammad Ghaffarian
  */
 public class AbstractSyntaxTree extends Graph<ASNode, ASEdge> {
+    
+    public final String FILE_PATH;
+    
+    public final ASNode ROOT;
 	
-	public AbstractSyntaxTree() {
+    /**
+     * Construct a new empty Abstract Syntax Tree, 
+     * for the given source-code file-path.
+     */
+	public AbstractSyntaxTree(String path) {
 		super(true);
+        FILE_PATH = path;
+        ROOT = new ASNode(ASNode.Type.ROOT);
+        addVertex(ROOT);
 	}
     
+    /**
+     * Copy constructor.
+     */
     public AbstractSyntaxTree(AbstractSyntaxTree ast) {
         super(ast);
+        ROOT = ast.ROOT;
+        FILE_PATH = ast.FILE_PATH;
+    }
+    
+	/**
+	 * Export this Abstract Syntax Tree (AST) to specified file format.
+	 */
+	public void export(String format, String outDir) throws FileNotFoundException {
+		switch (format.toLowerCase()) {
+			case "dot":
+				exportDOT(outDir);
+				break;
+			case "json":
+				exportJSON(outDir);
+				break;
+		}
+	}
+	
+	/**
+	 * Export this Abstract Syntax Tree (AST) to JSON format.
+	 * The JSON file will be saved inside the given directory path.
+	 */
+	public void exportJSON(String outDir) throws FileNotFoundException {
+        throw new UnsupportedOperationException("AST export to JSON not implemented yet!");
+    }
+    
+    /**
+     * Export this Abstract Syntax Tree (AST) to DOT format. 
+     * The DOT file will be saved inside the given directory. 
+     * The DOT format is mainly aimed for visualization purposes.
+     */
+    public void exportDOT(String outDir) throws FileNotFoundException {
+        String filename = new File(FILE_PATH).getName();
+        filename = filename.substring(0, filename.lastIndexOf('.'));
+        String filepath = outDir + filename + "-AST.dot";
+        try (PrintWriter dot = new PrintWriter(filepath, "UTF-8")) {
+            dot.println("digraph " + filename + "_AST {");
+            dot.println("  // graph-vertices");
+            Map<ASNode, String> nodeNames = new LinkedHashMap<>();
+            int nodeCounter = 1;
+            for (ASNode node : allVertices) {
+                String name = "n" + nodeCounter++;
+                nodeNames.put(node, name);
+                StringBuilder label = new StringBuilder("  [label=\"");
+                if (node.getLineOfCode() > 0)
+                    label.append(node.getLineOfCode()).append(":  ");
+                label.append(StringUtils.escape(node.toString())).append("\"];");
+                dot.println("  " + name + label.toString());
+            }
+			dot.println("  // graph-edges");
+            for (Edge<ASNode, ASEdge> edge : allEdges) {
+                String src = nodeNames.get(edge.source);
+                String trg = nodeNames.get(edge.target);
+                dot.println("  " + src + " -> " + trg + ";");
+            }
+			dot.println("  // end-of-graph\n}");
+        } catch (UnsupportedEncodingException ex) {
+			Logger.error(ex);
+        }
+		Logger.info("AST exported to: " + filepath);
     }
 	
 }
