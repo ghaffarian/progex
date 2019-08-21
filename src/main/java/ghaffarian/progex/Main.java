7cce937bc8cb59fd064c18d56b9b0840524a96ef
Seyed Mohammad Ghaffarian <s.m.ghaffarian@gmail.com>
2019-08-21 17:01:35 +0430

    Changed version to 3.2.0 in source-code

--- src/main/java/ghaffarian/progex/Main.java
+++ src/main/java/ghaffarian/progex/Main.java
@@ -10,7 +10,7 @@ import ghaffarian.nanologger.Logger;
  */
 public class Main {
 	
-	public static String VERSION = "3.0.0";
+	public static String VERSION = "3.2.0";
 
     /**
      * @param args the command line arguments
