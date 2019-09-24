7771d503feedb7e7f0da48f4c0078a6184fa863c
Seyed Mohammad Ghaffarian <ghaffarian@inside.sahab.ir>
2019-09-24 13:36:16 +0330

    Add AST JSON export

--- src/main/java/ghaffarian/progex/graphs/ast/AbstractSyntaxTree.java
+++ src/main/java/ghaffarian/progex/graphs/ast/AbstractSyntaxTree.java
@@ -12,6 +12,7 @@ import ghaffarian.progex.utils.StringUtils;
 import ghaffarian.nanologger.Logger;
 import ghaffarian.progex.graphs.AbstractProgramGraph;
 import java.io.IOException;
+import java.util.Map.Entry;
 
 /**
  * Abstract Syntax Tree (AST).
@@ -54,8 +55,7 @@ public class AbstractSyntaxTree extends AbstractProgramGraph<ASNode, ASEdge> {
             outDir += File.separator;
         File outDirFile = new File(outDir);
         outDirFile.mkdirs();
-        String filename = new File(filePath).getName();
-        filename = filename.substring(0, filename.lastIndexOf('.'));
+        String filename = fileName.substring(0, fileName.lastIndexOf('.'));
         String filepath = outDir + filename + "-AST.dot";
         try (PrintWriter dot = new PrintWriter(filepath, "UTF-8")) {
             dot.println("digraph " + filename + "_AST {");
@@ -111,8 +111,11 @@ public class AbstractSyntaxTree extends AbstractProgramGraph<ASNode, ASEdge> {
 				gml.println("    line " + node.getLineOfCode());
 				gml.println("    type \"" + node.getType() + "\"");
                 String code = node.getCode();
-				gml.println("    label \"" + (StringUtils.isEmpty(code) ? node.getType() : StringUtils.escape(code)) + "\"");
-				gml.println("    normalized \"" + StringUtils.escape(node.getNormalizedCode())+ "\"");
+                code = StringUtils.isEmpty(code) ? node.getType().label : StringUtils.escape(code);
+				gml.println("    label \"" + code + "\"");
+                String normalized = node.getNormalizedCode();
+                normalized = StringUtils.isEmpty(normalized) ? code : StringUtils.escape(normalized);
+				gml.println("    normalized \"" + normalized + "\"");
 				gml.println("  ]");
 				nodeIDs.put(node, nodeCounter);
 				++nodeCounter;
@@ -138,6 +141,61 @@ public class AbstractSyntaxTree extends AbstractProgramGraph<ASNode, ASEdge> {
 	
     @Override
 	public void exportJSON(String outDir) throws FileNotFoundException {
-        throw new UnsupportedOperationException("AST export to JSON not implemented yet!");
+        if (!outDir.endsWith(File.separator))
+            outDir += File.separator;
+        File outDirFile = new File(outDir);
+        outDirFile.mkdirs();
+		String filename = fileName.substring(0, fileName.indexOf('.'));
+		String filepath = outDir + filename + "-AST.json";
+		try (PrintWriter json = new PrintWriter(filepath, "UTF-8")) {
+			json.println("{\n  \"directed\": true,");
+			for (Entry<String, String> property: properties.entrySet()) {
+                switch (property.getKey()) {
+                    case "directed":
+                        continue;
+                    default:
+                        json.println("  \"" + property.getKey() + "\": \"" + property.getValue() + "\",");
+                }
+            }
+			json.println("  \"file\": \"" + fileName + "\",\n");
+			json.println("  \"nodes\": [");
+            //
+			Map<ASNode, Integer> nodeIDs = new LinkedHashMap<>();
+			int nodeCounter = 0;
+			for (ASNode node: allVertices) {
+				json.println("    {");
+				json.println("      \"id\": " + nodeCounter + ",");
+				json.println("      \"line\": " + node.getLineOfCode() + ",");
+				json.println("      \"type\": \"" + node.getType() + "\",");
+                String code = node.getCode();
+                code = StringUtils.isEmpty(code) ? node.getType().label : StringUtils.escape(code);
+				json.println("      \"label\": \"" + code + "\",");
+                String normalized = node.getNormalizedCode();
+                normalized = StringUtils.isEmpty(normalized) ? code : StringUtils.escape(normalized);
+				json.println("      \"normalized\": \"" + normalized + "\"");
+				json.println("    },");
+				nodeIDs.put(node, nodeCounter);
+				++nodeCounter;
+			}
+            //
+			json.println("  ],\n\n  \"edges\": [");
+			int edgeCounter = 0;
+			for (Edge<ASNode, ASEdge> edge: allEdges) {
+				json.println("    {");
+				json.println("      \"id\": " + edgeCounter + ",");
+				json.println("      \"source\": " + nodeIDs.get(edge.source) + ",");
+				json.println("      \"target\": " + nodeIDs.get(edge.target) + ",");
+				json.println("      \"label\": \"\"");  // TODO: should be 'edge.label'; 
+                // Java-AST-Builder uses Digraph::addEdge(V, V) which is addEdge(new Edge(V, null, V))!
+                // Using a null edge label can have its use-cases, but in this case we need something like
+                // Digraph::addDefaultEdge(V, V) which is addEdge(V, new E(), V) using a default constructor.
+				json.println("    },");
+				++edgeCounter;
+			}
+			json.println("  ]\n}");
+		} catch (UnsupportedEncodingException ex) {
+			Logger.error(ex);
+		}
+		Logger.info("AST exported to: " + filepath);
     }
 }
